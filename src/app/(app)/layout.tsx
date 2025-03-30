dceb8a2fcfd5d94d6aebcadbb6c662b665c1ac38
Duy Dang <d.dang@eml.com.au>
2025-03-31 00:31:24 +0700

    Add tawk.to

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -7,6 +7,7 @@ import Header from "~/components/header";
 import Footer from "~/components/footer";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "El.sneaker",
@@ -24,12 +25,27 @@ export default function RootLayout({
         className={`text-neutral-900 ${gilroy.variable} ${beVietnamPro.className}`}
       >
         <ClientProviders>
-            <Header />
-            <main>{children}</main>
-            <Footer />
+          <Header />
+          <main>{children}</main>
+          <Footer />
         </ClientProviders>
         <Analytics />
         <SpeedInsights />
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API = Tawk_API || {},
+                Tawk_LoadStart = new Date();
+            (function() {
+              var s1 = document.createElement("script"),
+                  s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = 'https://embed.tawk.to/67e4f224aab3cd1911b88047/1inb53o2b';
+              s1.charset = 'UTF-8';
+              s1.setAttribute('crossorigin', '*');
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
